3019fc8d2de7a94d1bef57182bf107eae05f7bc0
advisorslic <advisors.licofindia@gmail.com>
2026-01-12 23:32:39 +0530

    Update callback.js

--- functions/api/callback.js
+++ functions/api/callback.js
@@ -1,15 +1,11 @@
 export async function onRequestGet({ request, env }) {
   const url = new URL(request.url);
   const code = url.searchParams.get("code");
-
   if (!code) return new Response("Missing code", { status: 400 });
 
   const tokenRes = await fetch("https://github.com/login/oauth/access_token", {
     method: "POST",
-    headers: {
-      "Accept": "application/json",
-      "Content-Type": "application/json",
-    },
+    headers: { Accept: "application/json", "Content-Type": "application/json" },
     body: JSON.stringify({
       client_id: env.GITHUB_CLIENT_ID,
       client_secret: env.GITHUB_CLIENT_SECRET,
@@ -21,21 +17,21 @@ export async function onRequestGet({ request, env }) {
   const token = tokenJson.access_token;
 
   if (!token) {
-    return new Response("Token exchange failed: " + JSON.stringify(tokenJson), {
-      status: 400,
-    });
+    return new Response("Token exchange failed: " + JSON.stringify(tokenJson), { status: 400 });
   }
+
+  const ORIGIN = "https://advisorslic.in";
 
   const html = `<!doctype html><html><body>
 <script>
   (function () {
     var msg = "authorization:github:success:${token}";
-    if (window.opener) window.opener.postMessage(msg, "*");
+    if (window.opener) window.opener.postMessage(msg, "${ORIGIN}");
     window.close();
   })();
 </script>
-Authorized. You can close this tab.
+Authorized. You may close this window.
 </body></html>`;
 
-  return new Response(html, { headers: { "Content-Type": "text/html" } });
+  return new Response(html, { headers: { "Content-Type": "text/html; charset=utf-8" } });
 }
